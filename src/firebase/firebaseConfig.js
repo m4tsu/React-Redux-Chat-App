17eb00d5a830b032d23e30f395be01e9226c06f8
m4tsu <matsurec0147@gmail.com>
2019-09-03 23:25:55 +0900

    init firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAlwlCSJJPgIrb2ZpPf9lCnFpabbi04Wag",
+  authDomain: "react-redux-chat-app-1212c.firebaseapp.com",
+  databaseURL: "https://react-redux-chat-app-1212c.firebaseio.com",
+  projectId: "react-redux-chat-app-1212c",
+  storageBucket: "",
+  messagingSenderId: "697987466112",
+  appId: "1:697987466112:web:87ac480fd7bf520c"
+}
+
+export default firebaseConfig;
